b1457bd20fa4263e95c8850451c29c37464235d6
AnnaULV <razumanna@mail.ru>
2023-02-14 15:37:34 +0300

    ll

---
+++ js/newspage.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  "use strict";
+
+  if ($(".gallery > .swiper-container > .swiper > .swiper-wrapper > .swiper-slide").length > 0) {
+    new Swiper(".gallery > .swiper-container > .swiper", {
+      slidesPerView: 1,
+      pagination: {
+        el: "article > .section > .center > .swiper-pagination",
+        clickable: true
+      }
+    });
+  }
+
+  return false;
+});
