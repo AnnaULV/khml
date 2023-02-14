247843dce443f20dcb3e3c0341ad50a021fdb23f
AnnaULV <razumanna@mail.ru>
2023-02-14 16:30:40 +0300

    jhj

--- js/newspage.js
+++ js/newspage.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-  "use strict";
+    "use strict";
 
-  if ($(".gallery > .swiper-container > .swiper > .swiper-wrapper > .swiper-slide").length > 0) {
-    new Swiper(".gallery > .swiper-container > .swiper", {
-      slidesPerView: 1,
-      pagination: {
-        el: "article > .section > .center > .swiper-pagination",
-        clickable: true
-      }
-    });
-  }
+    if ($(".gallery > .swiper-container > .swiper > .swiper-wrapper > .swiper-slide").length > 0) {
+        new Swiper(".gallery > .swiper-container > .swiper", {
+            slidesPerView: 1,
+            spaceBetween: 8,
+            pagination: {
+                el: "article > .section > .center > .swiper-pagination",
+                clickable: true
+            }
+        });
+    }
 
-  return false;
+    return false;
 });
