b6d5eaccfeba3656a4dc46a15e15b9314f90879f
AnnaULV <razumanna@mail.ru>
2023-02-14 16:42:29 +0300

    Update newspage.js

--- js/newspage.js
+++ js/newspage.js
@@ -5,6 +5,17 @@ $(document).ready(function () {
         new Swiper(".gallery > .swiper-container > .swiper", {
             slidesPerView: 1,
             spaceBetween: 8,
+            breakpoints: {
+                481: {
+                    spaceBetween: 16
+                },
+                961: {
+                    spaceBetween: 24
+                },
+                1201: {
+                    spaceBetween: 32
+                }
+            },
             pagination: {
                 el: "article > .section > .center > .swiper-pagination",
                 clickable: true
