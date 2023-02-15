44844ca01f0e870f238c455100e38c7813657e76
AnnaULV <razumanna@mail.ru>
2023-02-15 13:50:03 +0300

    jhnj

--- js/photoline.js
+++ js/photoline.js
@@ -1,24 +1,23 @@
 $(document).ready(function () {
     "use strict";
 
-    if ($(".gallery.newspage > .swiper-container > .swiper > .swiper-wrapper > .swiper-slide").length > 0) {
-        new Swiper(".gallery.newspage > .swiper-container > .swiper", {
+    if ($(".gallery.photoline > .swiper-container > .swiper > .swiper-wrapper > .swiper-slide").length > 0) {
+        new Swiper(".gallery.photoline > .swiper-container > .swiper", {
             slidesPerView: 1,
             spaceBetween: 8,
             breakpoints: {
                 481: {
+                    slidesPerView: 2,
                     spaceBetween: 16
                 },
                 961: {
+                    slidesPerView: 3,
                     spaceBetween: 24
                 },
                 1201: {
+                    slidesPerView: 3,
                     spaceBetween: 32
                 }
-            },
-            pagination: {
-                el: "article > .section > .center > .swiper-pagination",
-                clickable: true
             }
         });
     }
